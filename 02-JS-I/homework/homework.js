1417129a74351917600f43d8afb017cab032c305
fabrizio <fabrisegat03@gmail.com>
2022-05-24 16:21:34 -0300

    hola

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,8 +1,7 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
-
+//
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = "hola";
-
+const nuevaString = "asdasd"
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = 1;
 
@@ -28,46 +27,56 @@ const nuevoModulo = 21 % 5 === 1;
 function devolverString(str) {
   // "Return" la string provista: str
   // Tu código:
-  
+  return str
 }
 
 function suma(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código:
-  
+  return x+y
 }
 
 function resta(x, y) {
   // Resta "y" de "x" y devuelve el valor
   // Tu código:
-  
+  return x-y
 }
 
 function multiplica(x, y) {
   // Multiplica "x" por "y" y devuelve el valor
   // Tu código:
-  
+  return x*y
 }
 
 function divide(x, y) {
   // Divide "x" entre "y" y devuelve el valor
   // Tu código:
-  
+  return x / y
 }
 
 function sonIguales(x, y) {
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+ // if (x ===y)return true
+ // return false
+if(x === y) {
+  return true
+}else {
+  return false
+}
 }
 
 function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  if (str1.length === str2.length) {
+    return true
+  } else {
+    return false 
+  }
 }
 
 function menosQueNoventa(num) {
